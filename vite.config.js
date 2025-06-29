9b075037267c7c5041208ee26442ccb2034837cb
rpuganda <mucunguziinno256@gmail.com>
2025-06-29 16:57:08 +0300

    Add netlify.toml for build config and redirects, remove old _redirects

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Add this base option:
-  base: './', // Or '/'
+  base: './', // <-- Make sure this line is present
 });
